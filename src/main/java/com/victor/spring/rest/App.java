1aa3abd870f24d04f9a911d8f0074b146d8a369a
Drobyshevskyy <victordrob35@gmail.com>
2023-08-03 13:45:26 +0300

    Initial commit

---
+++ src/main/java/com/victor/spring/rest/App.java
@@ -0,0 +1,36 @@
+package com.victor.spring.rest;
+
+import com.victor.spring.rest.configuration.MyConfig;
+import com.victor.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication"
+                , Communication.class);
+
+        /*List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);*/
+
+        /*Employee empByID = communication.getEmployee(1);
+        System.out.println(empByID);*/
+
+        /*Employee emp = new Employee("Masha", "Petrova"
+                , "HR", 1200);
+        emp.setId(10);
+        communication.saveEmployee(emp);*/
+
+        communication.deleteEmployee(10);
+
+    }
+}
